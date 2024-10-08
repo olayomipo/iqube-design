b77f4c0d41f8c1c823a83ea861f959193c90764d
olayomipo <adesoyeademola@hotmail.com>
2024-10-08 23:14:07 +0100

    fix component:mainBackgound

--- src/components/MainBackground.jsx
+++ src/components/MainBackground.jsx
@@ -4,7 +4,7 @@ import classes from './MainBackground.module.css';
 
 function MainBackground({ children }) {
   return (
-    <div className='min-h-screen bg-gray-100 flex items-center justify-center '>
+    <div className=' bg-gray-100'>
           { children }          
     </div>
   );
