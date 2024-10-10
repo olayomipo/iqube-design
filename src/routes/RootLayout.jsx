cb64a6ec4fddac38f346a82e9b06f23afb854560
olayomipo <adesoyeademola@hotmail.com>
2024-10-10 22:10:06 +0100

    Root Layout: minor fix (Naming issues)

--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -1,6 +1,6 @@
 
 import { Outlet } from 'react-router-dom';
-import MainBackground from '../components/MainBackground';
+import MainBackground from '../components/mainBackground';
 
 function RootLayout(){
     return(
